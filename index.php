94b50424f05253b8a8a5cbefbeae7bd629ea6d96
sadjith <sadjith@venatrix.co.in>
2018-08-07 22:11:01 +0530

    Update index.php

--- index.php
+++ index.php
@@ -62,7 +62,7 @@
             <li>Basic</li>
           </ol>
           <h1>Welcome to an OpenShift Application!</h1>
-          <p>The purpose of this application is to demonstrate BLUE - GREEN deployment.!</p>
+          <p>The purpose of this application is to Demonstrate BLUE - GREEN Deployment in Openshift</p>
         </div>
       </div>
       <div class="row">
