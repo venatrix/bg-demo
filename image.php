5b2be6b019cafea0049f11c21b40140d328ae00b
sadjith <sadjith@venatrix.co.in>
2018-09-25 11:19:39 +0530

    Update image.php

--- image.php
+++ image.php
@@ -3,12 +3,10 @@
 $im = imagecreatetruecolor(200, 200);
 $white = imagecolorallocate($im, 255, 255, 255);
 $blue = imagecolorallocate($im, 0, 0, 255);
-$green = imagecolorallocate($im, 0, 0, 255);
-
-// Draw a filled rectangle
-imagefilledrectangle($im, 0, 0, 199, 199, $blue);
-//imagefilledrectangle($im, 0, 0, 199, 199, $green);
-
+$green = imagecolorallocate($im, 0, 255, 0);
+// Draw a white rectangle
+//imagefilledrectangle($im, 0, 0, 199, 199, $blue);
+imagefilledrectangle($im, 0, 0, 199, 199, $green);
 // Save the image
 header('Content-Type: image/png');
 imagePNG($im);
